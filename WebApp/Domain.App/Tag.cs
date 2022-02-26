50f35d1f28d20ea8af5ea6daf703a71e413fc674
railmj <railmj@ttu.ee>
2022-02-26 21:10:32 +0200

    Fixed nullable reference types

--- WebApp/Domain.App/Tag.cs
+++ WebApp/Domain.App/Tag.cs
@@ -5,7 +5,7 @@ namespace Domain.App;
 public class Tag : IBaseItem
 {
     public Guid Id { get; set; }
-    public string Tagname { get; set; }
+    public string Tagname { get; set; } = default!;
 
     public ICollection<IdeaTag>? IdeaTags { get; set; }
     public ICollection<FeedTag>? FeedTags { get; set; }
